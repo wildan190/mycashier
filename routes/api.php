37c1e8ceb590e174141f1b6f3f507d4fddf04e22
Wildan <asep.wildan@lumoshive.com>
2024-06-26 10:10:58 +0700

    perbaikan app.layout

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,13 @@
 use App\Http\Controllers\API\Auth\LoginController;
 use App\Http\Controllers\API\Auth\LogoutController;
 use App\Http\Controllers\API\Auth\RegisterController;
+use App\Http\Controllers\API\DashboardController;
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\CustomerController;
+use App\Http\Controllers\API\TransactionController;
+use App\Http\Controllers\API\ReportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/register', [RegisterController::class, 'register']);
@@ -17,37 +18,37 @@ Route::post('/logout', [LogoutController::class, '__invoke']);
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified', 'CheckRole:superadmin'])->group(function () {
 
     Route::group(['prefix' => 'dashboard'], function () {
-        Route::get('/', [App\Http\Controllers\API\DashboardController::class, 'dashboard']);
+        Route::get('/', [DashboardController::class, 'dashboard']);
     });
 
     Route::group(['prefix' => 'products'], function () {
-        Route::get('/', [App\Http\Controllers\API\ProductController::class, 'index']);
-        Route::post('/', [App\Http\Controllers\API\ProductController::class, 'store']);
-        Route::get('/{id}', [App\Http\Controllers\API\ProductController::class, 'show']);
-        Route::put('/{id}', [App\Http\Controllers\API\ProductController::class, 'update']);
-        Route::delete('/{id}', [App\Http\Controllers\API\ProductController::class, 'destroy']);
+        Route::get('/', [ProductController::class, 'index']);
+        Route::post('/', [ProductController::class, 'store']);
+        Route::get('/{id}', [ProductController::class, 'show']);
+        Route::put('/{id}', [ProductController::class, 'update']);
+        Route::delete('/{id}', [ProductController::class, 'destroy']);
     });
 
     Route::group(['prefix' => 'customers'], function () {
-        Route::get('/', [App\Http\Controllers\API\CustomerController::class, 'index']);
-        Route::post('/', [App\Http\Controllers\API\CustomerController::class, 'store']);
-        Route::get('/{id}', [App\Http\Controllers\API\CustomerController::class, 'show']);
-        Route::put('/{id}', [App\Http\Controllers\API\CustomerController::class, 'update']);
-        Route::delete('/{id}', [App\Http\Controllers\API\CustomerController::class, 'destroy']);
+        Route::get('/', [CustomerController::class, 'index']);
+        Route::post('/', [CustomerController::class, 'store']);
+        Route::get('/{id}', [CustomerController::class, 'show']);
+        Route::put('/{id}', [CustomerController::class, 'update']);
+        Route::delete('/{id}', [CustomerController::class, 'destroy']);
     });
 
     Route::group(['prefix' => 'transactions'], function () {
-        Route::get('/', [App\Http\Controllers\API\TransactionController::class, 'index']);
-        Route::get('/{transaction}/print', [App\Http\Controllers\API\TransactionController::class, 'print']);
-        Route::post('/', [App\Http\Controllers\API\TransactionController::class, 'store']);
-        Route::get('/{transaction}', [App\Http\Controllers\API\TransactionController::class, 'show']);
-        Route::put('/{transaction}', [App\Http\Controllers\API\TransactionController::class, 'update']);
-        Route::delete('/{transaction}', [App\Http\Controllers\API\TransactionController::class, 'destroy']);
+        Route::get('/', [TransactionController::class, 'index']);
+        Route::get('/{transaction}/print', [TransactionController::class, 'print']);
+        Route::post('/', [TransactionController::class, 'store']);
+        Route::get('/{transaction}', [TransactionController::class, 'show']);
+        Route::put('/{transaction}', [TransactionController::class, 'update']);
+        Route::delete('/{transaction}', [TransactionController::class, 'destroy']);
     });
 
     Route::group(['prefix' => 'reports'], function () {
-        Route::get('/monthly', [App\Http\Controllers\API\ReportController::class, 'monthlyReport']);
-        Route::get('/products', [App\Http\Controllers\API\ReportController::class, 'productReport']);
+        Route::get('/monthly', [ReportController::class, 'monthlyReport']);
+        Route::get('/products', [ReportController::class, 'productReport']);
     });
 
 });
